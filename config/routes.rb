c903f26c343cdec0af1c15795a3c7a051e5f0d7b
Filipe Prado <fi-lipe2010@hotmail.com>
2016-10-07 16:07:30 -0300

    Mudando Root para Usuarios autenticados

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
 
-  root 'welcome#index'
+
 
   resources :docs
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  authenticated :user do
+    root "docs#index", as: "authenticated_root"
+  end
+
+
+  root 'welcome#index'
 end
